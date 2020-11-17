8c8e9490b1e91a86751c1bea3e12f0a7070f2f38
ManaliParab12 <manaliparab10@gmail.com>
2020-11-17 21:55:45 +0530

    Added Distance method to calculate length of line

--- src/lineComparison/LineComparison.java
+++ src/lineComparison/LineComparison.java
@@ -4,6 +4,14 @@ import java.util.Scanner;
 
 public class LineComparison {
 	
+	public static double distance (double x1, double y1, double x2, double y2) {
+		double x = Math.pow(x2 - x1, 2);
+		double y = Math.pow(y2 - y1, 2);
+		double dist = Math.sqrt(x + y);
+		
+		return dist;
+	}
+	
 	public static void main(String[] args) {
 		System.out.println("Welcome to line Comparison Computation Program ");
 		Scanner sc = new Scanner(System.in);
@@ -24,6 +32,8 @@ public class LineComparison {
 		dx=sc.nextDouble();
 		System.out.println("Enter dy Coordinates : ");
 		dy=sc.nextDouble();
+		double distAB = distance(ax, ay, bx, by);
+		double distCD = distance(cx, cy, dx, dy);
 	}
 
 }
