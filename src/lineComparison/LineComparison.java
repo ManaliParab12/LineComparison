97b0b6d06b8a67664fcac351127d713726bb9e1f
ManaliParab12 <manaliparab10@gmail.com>
2020-11-17 21:51:51 +0530

    Added Scanner method to take line coordinates from user

--- src/lineComparison/LineComparison.java
+++ src/lineComparison/LineComparison.java
@@ -1,9 +1,29 @@
 package lineComparison;
 
+import java.util.Scanner;
+
 public class LineComparison {
 	
 	public static void main(String[] args) {
 		System.out.println("Welcome to line Comparison Computation Program ");
+		Scanner sc = new Scanner(System.in);
+		double ax, ay, bx, by, cx, cy, dx, dy;
+		System.out.println("Enter ax Coordinates : ");
+		ax=sc.nextDouble();
+		System.out.println("Enter ay Coordinates : ");
+		ay=sc.nextDouble();
+		System.out.println("Enter bx Coordinates : ");
+		bx=sc.nextDouble();
+		System.out.println("Enter by Coordinates : ");
+		by=sc.nextDouble();
+		System.out.println("Enter cx Coordinates : ");
+		cx=sc.nextDouble();
+		System.out.println("Enter cy Coordinates : ");
+		cy=sc.nextDouble();		
+		System.out.println("Enter dx Coordinates : ");
+		dx=sc.nextDouble();
+		System.out.println("Enter dy Coordinates : ");
+		dy=sc.nextDouble();
 	}
 
 }
